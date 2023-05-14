0d616fb2a22a899437d25f487b48b349a81fec53
Artemides <edmpulasky@gmail.com>
2023-05-13 20:26:42 -0500

    add grafbase configs

--- src/app/api/auth/[...nextauth].tsx
+++ src/app/api/auth/[...nextauth].tsx
@@ -1,13 +1,37 @@
 import { AuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GithubAuthProvider from "next-auth/providers/github";
+import jsonwebtoken from "jsonwebtoken";
+import { JWT } from "next-auth/jwt";
+const authOptions: AuthOptions = {
+  providers: [
+    GithubAuthProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  jwt: {
+    encode: ({ secret, token }) =>
+      jsonwebtoken.sign(
+        {
+          ...token,
+          iss: "nextauth",
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 60,
+        },
+        secret
+      ),
+    decode:async({secret,token})=>jsonwebtoken.verify(token!,secret) as JWT
+  },
+  callbacks:{
+    async jwt({token,profile}){
+        if(profile) token.name=profile?.name!;
 
-const authOptions:AuthOptions={
-    providers:[
-        GithubAuthProvider({
-            clientId:process.env.GITHUB_CLIENT_ID!,
-            clientSecret:process.env.GITHUB_CLIENT_SECRET!
-        })
-    ]
-}
-export default NextAuth(authOptions)
+        return token;
+    },
+    session({session,token}){
+        if(token.name && session.user) session.user.name=token.name; //look out
+        return session
+    }
+  }
+};
+export default NextAuth(authOptions);
