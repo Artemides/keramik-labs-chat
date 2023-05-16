d29f68a45a211ffa58335f8f4a888819e7ea82d0
Artemides <edmpulasky@gmail.com>
2023-05-16 12:03:58 -0500

    setup intersection observer

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,6 +1,8 @@
 import { gql, useQuery } from "@apollo/client";
 import { Message } from "./Message";
 import type { Message as IMessage } from "./Message";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 const gerRecentMessageQuery = gql`
   query GetRecentMessages($last: Int) {
     messageCollection(last: $last) {
@@ -28,11 +30,48 @@ export const MessagesList = () => {
       variables: { last: 100 },
     }
   );
+
+  const [ref, inView, entry] = useInView({ trackVisibility: true, delay: 500 });
+
+  useEffect(() => {
+    if (!inView) return;
+    if (!entry || !entry.target) return;
+
+    entry.target.scrollIntoView({ behavior: "smooth" });
+  }, [data, inView, entry]);
+
+  if (loading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <p className="text-white">Fetching most recent chat messages.</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <p className="text-white">Something went wrong. Refresh to try again.</p>
+    );
+  }
+
   return (
     <div className="w-full flex flex-col space-y-3 overflow-y-scroll">
+      {!inView && (
+        <div className="absolute bottom-0 w-full flex justify-center  py-1.5 px-3 mb-[120px] inset-x-0 z-10 text-xs">
+          <button
+            className="py-1.5 px-3 text-xs bg-[#1c1c1f] border border-[#363739] rounded-full text-white font-medium"
+            onClick={() =>
+              entry?.target?.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Scroll to see the latest messages
+          </button>
+        </div>
+      )}
       {data?.messageCollection?.edges?.map(({ node }) => (
         <Message key={node.id} message={node} />
       ))}
+      <div ref={ref} />
     </div>
   );
 };
