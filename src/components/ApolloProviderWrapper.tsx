ef8d53bfd445abea46f15053ccf5fc0cd391b13b
Artemides <edmpulasky@gmail.com>
2023-05-16 12:43:22 -0500

    set up apollo client

--- src/components/ApolloProviderWrapper.tsx
+++ src/components/ApolloProviderWrapper.tsx
@@ -4,12 +4,19 @@ import {
   HttpLink,
   InMemoryCache,
   from,
+  split,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { SSELink, isLiveQuery } from "@grafbase/apollo-link";
+import { getOperationAST } from "graphql";
 import React, { PropsWithChildren, useMemo } from "react";
+
 const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_GRAFBASE_API_URL!,
 });
+
+const sseLink = new SSELink({ uri: process.env.NEXT_PUBLIC_GRAFBASE_API_URL! });
+
 export const ApolloProviderWrapper = ({ children }: PropsWithChildren) => {
   const client = useMemo(() => {
     const authMiddleWare = setContext(async (_, { headers }) => {
@@ -23,7 +30,16 @@ export const ApolloProviderWrapper = ({ children }: PropsWithChildren) => {
     });
 
     return new ApolloClient({
-      link: from([authMiddleWare, httpLink]),
+      link: from([
+        authMiddleWare,
+        split(
+          ({ query, operationName, variables }) =>
+            isLiveQuery(getOperationAST(query, operationName)),
+          sseLink,
+          httpLink
+        ),
+      ]),
+
       cache: new InMemoryCache(),
     });
   }, []);
