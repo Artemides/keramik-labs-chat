c7ddde5d846aacc748a30a2efb42bfdb10cadab9
Artemides <edmpulasky@gmail.com>
2023-05-16 12:31:56 -0500

    setup main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from 'next/image'
+import { MessageForm } from "@/components/MessageForm";
+import { MessagesList } from "@/components/MessageList";
+import { Navbar } from "@/components/Navbar";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
-  <main>
-    <h1>Hello keramik labs chat</h1>
-  </main>
-  )
+    <div className="flex flex-col">
+      <Navbar />
+      {session ? (
+        <>
+          <div className="flex-1 overflow-y-scroll p-6">
+            <div className="max-w-4xl mx-auto">
+              <div className="flex justify-between items-center">
+                <MessagesList />
+              </div>
+            </div>
+          </div>
+          <div className="p-6 bg-white/5 border-t border-[#363739]">
+            <div className="max-w-4xl mx-auto">
+              <MessageForm />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="h-full grid place-items-center">
+          <p className="text-lg md:text-2xl text-white">
+            Sign in to join the chat!
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
---
+++ src/components/MessageForm.tsx
@@ -0,0 +1,56 @@
+import { gql, useMutation } from "@apollo/client";
+import { useSession } from "next-auth/react";
+import { FormEvent, useState } from "react";
+
+const AddNewMessageMutation = gql`
+  mutation AddNewMessage($username: String!, $avatar: URL, $body: String!) {
+    messageCreate(
+      input: { username: $username, avatar: $avatar, body: $body }
+    ) {
+      message {
+        id
+      }
+    }
+  }
+`;
+export const MessageForm = () => {
+  const [AddNewMessage] = useMutation(AddNewMessageMutation);
+  const [body, setBody] = useState<string>("");
+  const { data: session } = useSession();
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!body) return;
+    AddNewMessage({
+      variables: {
+        username: session?.user?.name,
+        avatar: session?.user?.image,
+        body,
+      },
+    });
+    setBody("");
+  };
+  return (
+    <form
+      action=""
+      onSubmit={handleFormSubmit}
+      className="flex items-center space-x-3"
+    >
+      <input
+        autoFocus
+        id="message"
+        name="message"
+        placeholder="write a message..."
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        className="flex-1 h-12 px-3  rounded bg-[#222226] border border-[#222226] focus:border-[#222226] focus:outline-none text-white placeholder-white "
+      />
+      <button
+        type="submit"
+        className="bg-[#222226] rounded h-12 font-medium text-white w-24 text-lg border border-transparent"
+        disabled={!body || !session}
+      >
+        Send
+      </button>
+    </form>
+  );
+};
